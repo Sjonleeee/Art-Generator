e7844a951619dc0bb4b4394af9623d289769a41e
MinhTriHa <minh.tri.ha@student.howest.be>
2023-03-10 09:40:12 +0100

    Add spread operator in my project

--- src/App.jsx
+++ src/App.jsx
@@ -4,43 +4,71 @@ import Slider from "./components/Slider";
 import Drawing from "./components/Drawing.jsx";
 
 function App() {
-  const [radius, setRadius] = useState(10);
-  const [red, setRed] = useState(Math.floor(Math.random() * 256));
-  const [green, setGreen] = useState(Math.floor(Math.random() * 256));
-  const [blue, setBlue] = useState(Math.floor(Math.random() * 256));
-  const [x, setX] = useState(50);
-  const [y, setY] = useState(50);
+  const [style, setStyle] = useState({
+    radius: 10,
+    red: 10,
+    green: 100,
+    blue: 150,
+    x: 50,
+    y: 50,
+  });
+
+  const handleRadiusChange = (radius) => {
+    // Shorthand property names
+    setStyle({ ...style, radius });
+  };
+
+  const handleChannelChange = (colorChannel, value) => {
+    // Shorthand property names
+    setStyle({ ...style, [colorChannel]: value });
+  };
+
+  const handlePositionChange = (position, value) => {
+    // Shorthand property names
+    setStyle({ ...style, [position]: value });
+  };
+
+  const { radius, red, green, blue, x, y } = style;
 
   return (
     <div className="App">
       <Drawing radius={radius} color={{ red, green, blue }} x={x} y={y} />
 
-      <Slider label="Position X" value={x} onValueChange={(v) => setX(v)} />
-      <Slider label="Position Y" value={y} onValueChange={(v) => setY(v)} />
+      <Slider
+        label="Position X"
+        value={x}
+        onValueChange={(v) => handlePositionChange("x",v)}
+      />
+
+      <Slider
+        label="Position Y"
+        value={y}
+        onValueChange={(v) => handlePositionChange("y",v)}
+      />
 
       <Slider
         max={50}
         label="Radius"
         value={radius}
-        onValueChange={(v) => setRadius(v)}
+        onValueChange={(v) => handleRadiusChange(v)}
       />
       <Slider
         max={255}
         label="Red"
         value={red}
-        onValueChange={(v) => setRed(v)}
+        onValueChange={(v) => handleChannelChange("red", v)}
       />
       <Slider
         max={255}
         label="Green"
         value={green}
-        onValueChange={(v) => setGreen(v)}
+        onValueChange={(v) => handleChannelChange("green", v)}
       />
       <Slider
         max={255}
         label="Blue"
         value={blue}
-        onValueChange={(v) => setBlue(v)}
+        onValueChange={(v) => handleChannelChange("blue", v)}
       />
     </div>
   );
