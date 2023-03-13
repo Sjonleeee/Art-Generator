a5b029a7fca07fd3578d86c2a2e54bffc4a3d2a9
MinhTriHa <minh.tri.ha@student.howest.be>
2023-03-13 18:59:13 +0100

    Change strokeWidth into radius

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,16 @@ import Slider from "./components/Slider";
 
 const generateItem = () => ({
   y: Math.random() * 50,
-  radius: 5,
+  radius: 2,
   color: "#000000",
 });
 
-const defaultList = new Array(5).fill().map(() => generateItem());
+const defaultList = new Array(50).fill().map(() => generateItem());
 
 const COLORS = ["#000000", "#FF0000", "#00FF00", "#0000FF"];
 
 export default function App() {
-  const [radius, setRadius] = useState(10);
+  const [radius, setRadius] = useState(2);
   const [numItems, setNumItems] = useState(defaultList.length);
   const [items, setItems] = useState(defaultList);
   const [color, setColor] = useState(COLORS[0]);
@@ -62,7 +62,7 @@ export default function App() {
       <Slider
         value={radius}
         onValueChange={handleRadiusChange}
-        min={5}
+        min={2}
         max={20}
         label="Radius"
       />
@@ -70,8 +70,8 @@ export default function App() {
         value={numItems}
         onValueChange={handleNumItemsChange}
         label="Number of items"
-        min={1}
-        max={100}
+        min={50}
+        max={1000}
       />
 
       <Drawing items={items} color={color} radius={radius} />
--- src/components/Drawing.jsx
+++ src/components/Drawing.jsx
@@ -2,22 +2,28 @@ import React from "react";
 import "../Drawing.css";
 
 const Drawing = ({ items, color, radius }) => {
-  const canvasWidth = 800;
-  const canvasHeight = 800;
+  const canvasWidth = 80;
+  const canvasHeight = 80;
   const gridSize = Math.ceil(Math.sqrt(items.length));
-  const gridItemSize = canvasWidth / gridSize;
+  const gridItemSize = Math.min(
+    canvasWidth / gridSize,
+    canvasHeight / gridSize
+  );
 
   const getItemPosition = (index) => {
-    const maxWidth = canvasWidth / gridSize;
-    const maxHeight = canvasHeight / gridSize;
-
     // Calculate a random width and height for the rectangle
-    const width = Math.floor(Math.random() * (maxWidth - 10)) + 10;
-    const height = Math.floor(Math.random() * (maxHeight - 10)) + 10;
+    const width =
+      Math.floor(Math.random() * 4 + 1) * (gridItemSize / 4) + radius * 2;
+    const height =
+      Math.floor(Math.random() * 4 + 1) * (gridItemSize / 4) + radius * 2;
 
-    // Calculate the position of the rectangle
-    const x = (index % gridSize) * (maxWidth - width);
-    const y = Math.floor(index / gridSize) * (maxHeight - height);
+    // Calculate the position of the rectangle within the grid
+    const gridX = index % gridSize;
+    const gridY = Math.floor(index / gridSize);
+
+    // Calculate the position of the rectangle within the canvas
+    const x = gridX * gridItemSize + (gridItemSize - width) / 2;
+    const y = gridY * gridItemSize + (gridItemSize - height) / 2;
 
     return { x, y, width, height };
   };
@@ -35,18 +41,18 @@ const Drawing = ({ items, color, radius }) => {
   return (
     <svg viewBox={`0 0 ${canvasWidth} ${canvasHeight}`} className="Drawing">
       {items.map((item, index) => {
-        const { x, y } = getItemPosition(index);
+        const { x, y, width, height } = getItemPosition(index);
         const randomFill = getRandomColor();
         return (
           <rect
             key={index}
-            x={x}
-            y={y}
-            width={gridItemSize}
-            height={gridItemSize}
+            x={x - radius}
+            y={y - radius}
+            width={width}
+            height={height}
             fill={randomFill}
             stroke={color}
-            strokeWidth={radius}
+            radius={-radius}
           />
         );
       })}
